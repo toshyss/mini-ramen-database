e2ccd1a6a71da6c16378449eea32dad50a5d819e
Toshiya Sasaki <toshy.ss@gmail.com>
2019-04-09 20:50:32 +0900

    wip graphql schema

--- server.js
+++ server.js
@@ -1,9 +1,39 @@
 const express = require("express");
 const path = require("path");
+const graphqlHTTP = require("express-graphql");
+const { buildSchema } = require("graphql");
+
+const data = ("./ramen.json");
+
+const schema = buildSchema(`
+
+    type ramen {
+        menu: String
+        picture: String
+        shop: String
+        address: String
+        price: Int
+        taste: String
+        men-type: String
+        evaluation: Int
+        topping: String
+        memo: String
+    }
+`);
+
 
 const app = express();
 
 app.use(express.static(path.join(__dirname + "/dist")));
+
+app.use(
+    "/graphql",
+    graphqlHTTP({
+        schema,
+        rootValue: root,
+        graphiql: true,
+    })
+);
 
 const port = process.env.PORT || 4000;
 
